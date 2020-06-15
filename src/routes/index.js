51fc71aca850402694b07ec757d98760db6e7893
daniel mamani <dani.oscar.257@gmail.com>
2020-06-14 21:33:05 -0300

    OK: mostrar una tabla con los datos de mongodb

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,26 @@ const bodyParser = require('body-parser');
 const repo = require('../lib/repo');
 const config = require('../config/config');
 
+//---------------------------------------------------------------------
+//S: lib
+//TODO: mover a lib o utils
+function objFilter(obj, filters){ //U:recibe los filters para limpiar el obj pasado como parametro
+  let newObj={};
+  for( let key in  obj){
+    if(!filters.includes(key))
+      newObj[key]= obj[key]
+  }
+  return newObj;
+}
+
+function ArrayObjFilter( arrObj, filters ){
+  return arrObj.map( obj=> objFilter(obj, filters))
+}
+
+
+//--------------------------------------------------------------------
+//S: functions
+
 //Devuelve un objeto en el que agrega URL, Protocolo y TimeStamp
 function addReqInfo(message, req) {
     const timeStamp = Date.now();
@@ -16,14 +36,45 @@ function addReqInfo(message, req) {
     return message;
 }
 
+function dataGet(){
+  return new Promise((resolve,reject)=>{
+    repo.list(config.messageCollectionName)
+      .then(result => resolve(result))
+      .catch(err => {
+          console.log("err /api/getlist: ", err);
+          reject(err)
+      });
+  })
+}
+
+
+
 //Endpoints de la pagina:
 //Paginated Table
 router.get('/table', async(req, res) => {
-    //TODO: Muestro una pagina con una tabla paginada que muestra lo que hay en la bd
+  //1-obtener un array de datos que quiero mostrar en la tabla
+  //2- formatearlo para que la tabla en la view lo pueda usar
+  //3-pasarle el array en la view
+  //MEJORAS:
+  //1-le paso la cantidad total de documentos 
+  //2-agregar paginacion
+  //3-que la view redireccione a este mismo endpoint con el numero de paginacion que clickea el user
+
+  //1-obtener un array de datos que quiero mostrar en la tabla
+  let filters= ["serverReceivedTS", "protocol", "url", "serverEnqueuedTS"]//U: que keys no quiero mostrar en mi tabla
+  let headers= ['Id' , 'Fecha', 'Mensaje', 'Codigo', 'Latitud', 'Logitud', 'Interno', 'Patente'] //A: los titulos de la tabla que mostramos para los mensajes que nos llegaron
+  
+  let data= await dataGet();
+  result= ArrayObjFilter(data.result, filters)
+  let messages= result.map( result => Object.values(result)) //A: array de arrays con los values del diccioonario devuelto por la base de datos para darselo a la tabla 
+  let messagesCount= data.count;
+  res.render('messageTable',{rows: messages, messagesCount, headers})
+
 });
 
 //Endpoints de las APIS:
 //Recivo un evento y lo encolo
+//TODO: agregar error handler cuando el body es un JSON invalido
 router.post('/api/add', bodyParser.text({ type: '*/*' }), async(req, res, next) => {
     // "addReqInfo" agrega el timeStamp, protocolo y url al objeto "message" ademas de lo q viene en el "req.body"
     const message = addReqInfo(JSON.parse(req.body), req);
