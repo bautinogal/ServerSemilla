e379a607c79206163741305a9f7b29186d08569f
bautinogal <jbnogal@gmail.com>
2020-08-06 01:15:18 -0300

    levanto filtros

--- src/lib/mongodb/mongoDbHelpers.js
+++ src/lib/mongodb/mongoDbHelpers.js
@@ -1,5 +1,11 @@
 var MongoPool = require('./mongoDbConfig');
 
+function formatQuery(query) {
+    query = query || {};
+    if (typeof query === 'string') query = JSON.parse(query);
+    return query;
+}
+
 async function save(collectionName, document) { //U: guardar un documento en la colleccion pasada, devuelve una promesa
     let db = await MongoPool.getInstance(); //A: obtengo una coneccion existente o creo una nueva con la db
     if (typeof(document) === "object") {
@@ -13,52 +19,19 @@ async function save(collectionName, document) { //U: guardar un documento en la 
 
 function get(collectionName, query, queryOptions) {
 
-    //Si me vienen vacios los parametres les asigno un objeto vacio
-    if (query == null)
-        query = {};
+    query = formatQuery(query);
+    queryOptions = formatQuery(queryOptions);
 
-    if (queryOptions == null)
-        queryOptions = {};
+    return MongoPool.getInstance().collection(collectionName).find(query, queryOptions).toArray();
 
-    //Si me vienen strings los convierto a objetos que es lo que quiere mongo
-    if (typeof query === 'string')
-        query = JSON.parse(query);
-    if (typeof queryOptions === 'string')
-        queryOptions = JSON.parse(queryOptions);
-
-
-    return MongoPool.getInstance()
-        .then(db => db.collection(collectionName).find(query, queryOptions).toArray())
-        .catch(err => console.log("get error: %s", err));
-
-    return new Promise(async(resolve, reject) => {
-        let db = await MongoPool.getInstance();
-        db.collection(collectionName).find(query, queryOptions)
-            .then(res => res)
-            .toArray(async(err, result) => {
-                if (err)
-                    reject(err);
-                resolve(result.toString());
-            });
-    })
 }
 
 function getCount(collectionName, query, queryOptions) {
 
-    //Si me vienen vacios le agrego un objeto vacio si son strings los parseo
-    if (query == null)
-        query = {};
-    else if (typeof query === 'string')
-        query = JSON.parse(query);
-
-    if (queryOptions == null)
-        queryOptions = {};
-    else if (typeof queryOptions === 'string')
-        queryOptions = JSON.parse(queryOptions);
-
+    query = formatQuery(query);
+    queryOptions = formatQuery(queryOptions);
 
     return MongoPool.getInstance().collection(collectionName).count(query, queryOptions);
-
 
 }
 
--- tests/fillCollection.js
+++ tests/fillCollection.js
@@ -16,8 +16,23 @@ const post = () => {
     });
 }
 
-
-for (let index = 0; index < 12; index++) {
-    setTimeout(post, 1000);
-    element.num = element.num + 1;
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
+
+function myStopFunction() {
+    clearInterval(func);
+}
+
+var time = 110;
+var func = setInterval(function() {
+    element.fecha = randomDate(new Date(2012, 0, 1), new Date());
+    element.interno = Math.trunc(Math.random() * 2000);
+    element.codigo = Math.trunc(Math.random() * 200);
+    element.descripcion = "Esto es una descripcion de ejemplo max 90 alcanzada 120";
+    post();
+    if (time < 0) {
+        myStopFunction();
+    }
+    time--;
+}, 10);
