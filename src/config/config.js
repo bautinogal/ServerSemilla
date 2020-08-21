319780fc052c196bf8c1624a804ebdbcf61857e2
jbnogal@ventum-software.com <jbnogal@ventum-software.com>
2020-08-21 13:16:10 -0300

    config de la url de la cola

--- src/config/config.js
+++ src/config/config.js
@@ -11,6 +11,8 @@ const config = {
     jwtDfltExpires: process.env.JWT_DURATION || 3600, // Cuanto duran los tokens por dflt en segundos
     saltWorkFactor: process.env.SALT_WORK_FACTOR || 10, //A: las vueltas que usa bcrypt para encriptar las password
 
+    queueUrl: process.env.AMQP_URI || 'amqp://localhost',
+
     mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017', //URL del Cluster de MongoDb
     usersDB: process.env.USERS_DB || 'test',
     usersCollection: process.env.USERS_COLLECTION || 'users',
