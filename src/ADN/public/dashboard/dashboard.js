72ad470c1c0512d4f65e41346e65546882d54dee
jbnogal@ventum-software.com <jbnogal@ventum-software.com>
2020-09-17 00:25:56 -0300

    tajeta con shadow

--- src/ADN/public/dashboard/dashboard.js
+++ src/ADN/public/dashboard/dashboard.js
@@ -1,138 +1,8 @@
 import utils from '../lib/utils.js';
 import math from '../lib/math.js';
 import anim from '../lib/anim.js';
-console.log("WAD");
-/*
-const collection = "collection1"; //Coleccion de la db de donde vamos a sacar la data para mostrar
-const headers = { //Columnas de la tabla, formato : {'Title','name'}
-    'Fecha': 'fecha',
-    'Código': 'codigo',
-    'Descripción': "descripcion",
-    'Ubicación': 'ubicacion',
-    'Interno': 'interno',
-    'Patente': 'patente'
-};
-const dataMapping = {
+import table from './table/table.js';
 
-};
-
-//Referencias al documento--------------------------------
-const table = document.getElementById('dashboard-table');
-const tableHeaders = table.getElementsByTagName('thead')[0];
-const tableBody = table.getElementsByTagName('tbody')[0];
-const pagination = document.getElementById('dashboard-table-pagination');
-const filters = document.getElementById('dashboard-table-filters');
-const filterBtn = document.getElementById('filter-submit-btn');
-
-const getPage = () => {
-    return 0;
-}
-
-const getQuery = () => {
-    var result = {};
-    var formData = new FormData(filters);
-    for (var pair of formData.entries()) {
-        if (pair[1] != null && pair[1] != "")
-            result[pair[0]] = pair[1];
-    }
-    console.log(result);
-    return result;
-}
-
-const getQueryOptions = () => {
-    var page = getPage(); // Página en la que nos encontramos actualmente
-    return {
-        "limit": 10,
-        "skip": page * 10
-    };
-}
-
-const mapDataToHeaders = (data) => {
-    data.forEach(element => {
-        if ('fecha' in element) {
-            var date = new Date(element.fecha);
-            var year = date.getFullYear();
-            var month = date.getMonth();
-            if (month < 10)
-                month = "0" + month;
-            var day = date.getDate();
-            if (day < 10)
-                day = "0" + day;
-            var hours = date.getHours();
-            if (hours < 10)
-                hours = "0" + hours;
-            var minutes = date.getMinutes();
-            if (minutes < 10)
-                minutes = "0" + minutes;
-            var seconds = date.getSeconds();
-            if (seconds < 10)
-                seconds = "0" + seconds;
-            var fecha = year + "-" + month + "-" + day;
-            var hora = hours + ":" + minutes;
-            for (let index = fecha.length; index <= 16; index++) {
-                fecha += "\u00A0\u00A0";
-            }
-            element.fecha = fecha + hora;
-        }
-    });
-    return data;
-}
-
-const updateTable = (data) => {
-    console.log("dashboard@drawTable  data: %s", data);
-
-    tableHeaders.innerHTML = [];
-    let tr = document.createElement('tr');
-    tableHeaders.appendChild(tr);
-    Object.keys(headers).forEach(element => {
-        let header = document.createElement('th');
-        header.innerHTML = element;
-        tr.appendChild(header);
-    });
-
-    tableBody.innerHTML = [];
-    Object.values(data).forEach(element => {
-        let tr = document.createElement('tr');
-        tableBody.appendChild(tr);
-        Object.values(headers).forEach(header => {
-            let td = document.createElement('td');
-            if (header in element)
-                td.innerHTML = element[header];
-            else
-                td.innerHTML = '-';
-            tr.appendChild(td);
-        });
-    });
-
-}
-
-const updatePagination = () => {
-
-}
-
-const updateView = () => {
-    var query = getQuery();
-    var queryOptions = getQueryOptions();
-
-    utils.getData(collection, query, queryOptions)
-        .then((data) => {
-            data = mapDataToHeaders(data);
-            updateTable(data);
-            return utils.getCount(collection, query);
-        })
-        .then((totalCount) => {
-            updatePagination(totalCount); // Cantidad de resultados que coinciden con el query en la db
-        })
-        .catch((err) => console.log(err));
-}
-
-updateView();
-
-filterBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    updateView();
-})
-*/
 //--------------------------Dashboard---------------------------------------
 var contentDiv; //Contenedor donde voy a dibujar cada pantalla...
 
@@ -151,10 +21,10 @@ const dflt = {
                 var test = document.createElement("div");
                 test.id = "test";
                 test.style.margin = 'auto';
-                test.style.marginTop = (parent.offsetHeight * 0.05) + 'px';
-                test.style.marginBottom = (parent.offsetHeight * 0.05) + 'px';
-                test.style.marginLeft = (parent.offsetWidth * 0.05) + 'px';
-                test.style.marginRight = (parent.offsetWidth * 0.05) + 'px';
+                // test.style.marginTop = (parent.offsetHeight * 0.05) + 'px';
+                // test.style.marginBottom = (parent.offsetHeight * 0.05) + 'px';
+                // test.style.marginLeft = (parent.offsetWidth * 0.05) + 'px';
+                // test.style.marginRight = (parent.offsetWidth * 0.05) + 'px';
                 test.style.width = (parent.offsetWidth * 0.5) + 'px';
                 test.style.height = (parent.offsetHeight * 0.5) + 'px';
                 test.style.color = 'red';
@@ -167,7 +37,18 @@ const dflt = {
             name: "INGRESO",
         },
         basedatos: {
-            name: "BD",
+            name: "TABLE",
+            html: (parent) => {
+                var test = document.createElement("div");
+                test.id = "test";
+                test.style.margin = 'auto';
+                test.style.width = parent.offsetWidth + 'px';
+                test.style.height = parent.offsetHeight + 'px';
+                test.style.backgroundColor = 'transparent';
+                parent.appendChild(test);
+                table.create({}, test);
+                return test;
+            }
         },
     }
 }
@@ -308,8 +189,8 @@ const createSidebar = (data) => {
     var result = {};
     var sidebar = document.createElement("div");
     sidebar.id = data.id + "-sidebar";
-    sidebar.className = 'ventum-sidebar';
-
+    sidebar.className = 'ventum-sidebar shadow';
+    sidebar.style.zIndex = 9999;
     sidebar.appendChild(companyInfo());
     sidebar.appendChild(separatorLine());
     sidebar.appendChild(userInfo());
@@ -383,6 +264,7 @@ const addZoomInOut = (data) => {
 }
 
 const init = (data) => {
+    console.log("hola");
     var result = {};
     data = utils.fillObjWithDflt(data, dflt);
 
@@ -398,8 +280,8 @@ const init = (data) => {
 
     contentDiv = content.mainDiv;
 
-
     result.html = nav.result;
+    console.log("chau");
     return result;
 }
 
